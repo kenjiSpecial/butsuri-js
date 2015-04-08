2838d88c9594d9f7d30d7f0a163e787d8f681fe1
kenji-special <k.saito.1985@gmail.com>
2015-04-08 13:58:31 +0900

    change the directory of app and update the components.

--- src/js/components/ball.js
+++ src/js/components/ball.js
@@ -1,42 +1,72 @@
 var constants = require('./constants.js');
 var Vector2 = require('ks-vector').Vector2;
 var RigidBody = require('./rigid-body.js');
-var Floor = require('./floor.js');
+var Floor = require('./rectangle.js');
 var Contact = require('./contact.js');
 var utils   = require('./utils.js');
 
 var Ball = function( _mass, _rad, _pos, _vel ) {
   RigidBody.call(this, _mass, _rad, _rad, _pos, _vel)
-  this.radius = _rad;
+  this.rad = _rad;
+
+  if(this.invMass > 0){
+    var I = this.mass * this.rad * this.rad / 4;
+    this.invI = 1 / I;
+    console.log(this.invI);
+  }else{
+    this.invI = 0;
+  }
 
 };
 
-Ball.prototype = Object.create(Ball.prototype);
+Ball.prototype = Object.create(RigidBody.prototype);
 Ball.prototype.constructor = Ball;
 
 
 Ball.prototype.update = function( dt ) {
 
   RigidBody.prototype.setGravity.call(this);
+
+  this.vel.x += this.force.x * this.invMass;
+  this.vel.y += this.force.y * this.invMass;
+
   RigidBody.prototype.update.call(this, dt);
 
 }
 
 Ball.prototype.draw = function(ctx) {
 
+  ctx.save();
+
+  ctx.translate(this.pos.x, this.pos.y);
+  ctx.rotate(this.angle);
+
   ctx.fillStyle = "#000000"
   ctx.beginPath();
-  ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
+  ctx.arc( 0, 0, this.rad, 0, 2 * Math.PI);
   ctx.fill();
 
+  ctx.strokeStyle = "#ffffff";
+  ctx.beginPath();
+  ctx.moveTo( -5, 0);
+  ctx.lineTo(  5, 0);
+  ctx.stroke();
 
-  if(this.pos.x > window.innerWidth + this.radius * 2 || this.pos.x < 0 - this.radius * 2 || this.pos.y + this.radius*2 > window.innerHeight + this.rad * 2){
+  ctx.beginPath();
+  ctx.moveTo(0, -5);
+  ctx.lineTo(0, 5);
+  ctx.stroke();
+
+  ctx.restore();
+
+
+  if(this.pos.x > window.innerWidth + this.rad * 2 || this.pos.x < 0 - this.rad * 2 || this.pos.y + this.rad*2 > window.innerHeight + this.rad * 2){
     this.reset();
   }
 };
 
 Ball.prototype.reset = function() {
-  this.pos = new Vector2(window.innerWidth/2 - 100 + 200 * Math.random(), -this.radius * 2 - 400 * Math.random());
+  this.pos = new Vector2(window.innerWidth/2 - 100 + 200 * Math.random(), -this.rad * 2 - 400 * Math.random());
   this.vel = new Vector2();
 }
 
@@ -59,15 +89,15 @@ Ball.prototype.getClosestPoints = function(rBody) {
 
     // generate closes points
     var pa = new Vector2();
-    pa.x = ballA.pos.x + n.x * ballA.radius;
-    pa.y = ballA.pos.y + n.y * ballA.radius;
+    pa.x = ballA.pos.x + n.x * ballA.rad;
+    pa.y = ballA.pos.y + n.y * ballA.rad;
 
     var pb = new Vector2();
-    pb.x = ballB.pos.x - n.x * ballB.radius;
-    pb.y = ballB.pos.y - n.y * ballB.radius;
+    pb.x = ballB.pos.x - n.x * ballB.rad;
+    pb.y = ballB.pos.y - n.y * ballB.rad;
 
     // getdistance
-    var dist = delata.getLength() - (ballA.radius + ballB.radius);
+    var dist = delata.getLength() - (ballA.rad + ballB.rad);
 
     contacts.push(new Contact( ballA, ballB, pa, pb, n, dist ));
 
@@ -82,7 +112,10 @@ Ball.prototype.getClosestPoints = function(rBody) {
   }
 
   return contacts;
-}
+};
+
+
+
 
 
 module.exports = Ball;
---
+++ src/js/old/speculative-contacts-angular-hard-case/components/solver.js
@@ -0,0 +1,63 @@
+var numInteraction = 1;
+var solveType;
+
+var CONSTANTS = require('./constants.js');
+
+var solver = function(contacts) {
+    for (var jj = 0; jj < numInteraction; jj++) {
+        for (var ii = 0; ii < contacts.length; ii++) {
+            var con = contacts[ii];
+            var n = con.normal;
+            var relNv = con.getVelPb().subtract(con.getVelPa()).dotProduct(n);
+
+            speculativeSolver(con, n, relNv);
+
+        }
+    }
+}
+
+function speculativeSolver(con, n, relNv) {
+    var remove = relNv +   con.Dist / CONSTANTS.timeStep;
+
+    if (remove < 0) {
+        //console.log(1 / (con.A.invMass + con.B.invMass));
+        var mag = remove *  con.invDenom; // (con.A.invMass + con.B.invMass);
+        var imp = n.copy().multiply(mag );
+
+        con.applyImpulses(imp);
+    }
+}
+
+/**
+*/
+function discreteSolver ( con, n, relNv ) {
+  var remove = relNv + .4 * (con.Dist + 1) / CONSTANTS.timeStep;
+
+  if(con.mDist < 0 && remove < 0){
+
+    var mag = remove / (con.A.invMass + con.B.invMass);
+    var imp = con.normal.copy().multiply(mag);
+
+    con.applyImpulses(imp);
+  }
+}
+
+function discreteSequential(con, n, relNv){
+  if(con.mDist < 0){
+    var remove = relNv + .4 * (con.mDist + 1) / CONSTANTS.timeStep;
+
+    var mag = remove / (con.mA.invMass + con.mB.invMass);
+    var newImpulse = Math.min(mag + con.mImpulse, 0)
+    var change = newImpulse - con.mImpulse;
+
+    var imp = con.normal.copy().multiply(change);
+
+    con.applyImpulses(imp);
+
+    con.mImpulse = newImpulse;
+
+  }
+}
+
+
+module.exports = solver;
